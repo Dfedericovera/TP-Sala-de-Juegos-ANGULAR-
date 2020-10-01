1bbdc073e29157737195ca78da09ce74e7bda24c
Fede <dfedericovera@gmail.com>
2020-10-01 20:37:54 -0300

    memotest in progress

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
-import {Subscription} from "rxjs";/* 
+import { Subscription } from "rxjs";/* 
 import {TimerObservable} from "rxjs/observable/TimerObservable"; */
 @Component({
   selector: 'app-login',
@@ -12,19 +13,29 @@ export class LoginComponent implements OnInit {
 
   private subscription: Subscription;
   usuario = '';
-  clave= '';
+  clave = '';
   progreso: number;
-  progresoMensaje="esperando..."; 
-  logeando=true;
-  ProgresoDeAncho:string;
+  progresoMensaje = "esperando...";
+  logeando = true;
+  ProgresoDeAncho: string;
 
-  clase="progress-bar progress-bar-info progress-bar-striped ";
+  clase = "progress-bar progress-bar-info progress-bar-striped ";
+  
+  contrasena: any;
+  IngresarComoAdmin(f: NgForm) {
+    this.usuario = "Admin";
+    this.contrasena = "admin";
+  }
+
+  login() {
+
+  }
 
   constructor(
     private route: ActivatedRoute,
     private router: Router) {
-      this.progreso=0;
-      this.ProgresoDeAncho="0%";
+    this.progreso = 0;
+    this.ProgresoDeAncho = "0%";
 
   }
 
@@ -36,45 +47,45 @@ export class LoginComponent implements OnInit {
       this.router.navigate(['/Principal']);
     }
   }
-/*   MoverBarraDeProgreso() {
-    
-    this.logeando=false;
-    this.clase="progress-bar progress-bar-danger progress-bar-striped active";
-    this.progresoMensaje="NSA spy..."; 
-    let timer = TimerObservable.create(200, 50);
-    this.subscription = timer.subscribe(t => {
-      console.log("inicio");
-      this.progreso=this.progreso+1;
-      this.ProgresoDeAncho=this.progreso+20+"%";
-      switch (this.progreso) {
-        case 15:
-        this.clase="progress-bar progress-bar-warning progress-bar-striped active";
-        this.progresoMensaje="Verificando ADN..."; 
-          break;
-        case 30:
-          this.clase="progress-bar progress-bar-Info progress-bar-striped active";
-          this.progresoMensaje="Adjustando encriptación.."; 
-          break;
-          case 60:
-          this.clase="progress-bar progress-bar-success progress-bar-striped active";
-          this.progresoMensaje="Recompilando Info del dispositivo..";
-          break;
-          case 75:
-          this.clase="progress-bar progress-bar-success progress-bar-striped active";
-          this.progresoMensaje="Recompilando claves facebook, gmail, chats..";
-          break;
-          case 85:
-          this.clase="progress-bar progress-bar-success progress-bar-striped active";
-          this.progresoMensaje="Instalando KeyLogger..";
-          break;
-          
-        case 100:
-          console.log("final");
-          this.subscription.unsubscribe();
-          this.Entrar();
-          break;
-      }     
-    });
-  } */
+  /*   MoverBarraDeProgreso() {
+      
+      this.logeando=false;
+      this.clase="progress-bar progress-bar-danger progress-bar-striped active";
+      this.progresoMensaje="NSA spy..."; 
+      let timer = TimerObservable.create(200, 50);
+      this.subscription = timer.subscribe(t => {
+        console.log("inicio");
+        this.progreso=this.progreso+1;
+        this.ProgresoDeAncho=this.progreso+20+"%";
+        switch (this.progreso) {
+          case 15:
+          this.clase="progress-bar progress-bar-warning progress-bar-striped active";
+          this.progresoMensaje="Verificando ADN..."; 
+            break;
+          case 30:
+            this.clase="progress-bar progress-bar-Info progress-bar-striped active";
+            this.progresoMensaje="Adjustando encriptación.."; 
+            break;
+            case 60:
+            this.clase="progress-bar progress-bar-success progress-bar-striped active";
+            this.progresoMensaje="Recompilando Info del dispositivo..";
+            break;
+            case 75:
+            this.clase="progress-bar progress-bar-success progress-bar-striped active";
+            this.progresoMensaje="Recompilando claves facebook, gmail, chats..";
+            break;
+            case 85:
+            this.clase="progress-bar progress-bar-success progress-bar-striped active";
+            this.progresoMensaje="Instalando KeyLogger..";
+            break;
+            
+          case 100:
+            console.log("final");
+            this.subscription.unsubscribe();
+            this.Entrar();
+            break;
+        }     
+      });
+    } */
 
 }
